4e03007eb6e7efeba40e65d2c35206249f182831
Pedro1697 <agpn16@gmail.com>
2025-08-24 11:27:19 +0200

    First Commint

---
+++ main.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("Hi ReAct LangGraph with Function Calling")
+    print(os.getenv("GOOGLE_API_KEY"))
